259e7189f7a7882fbbadab5829fc1ff9bf8f61d9
Rahulpatel1217 <send2rahul1217@gmail.com>
2024-08-01 09:37:19 +0530

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { CategoryListComponent } from './Feature/category/category-list/category-list.component';
+import { AddCategoryComponent } from './Feature/category/add-category/add-category.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'admin/categories',
+        component:CategoryListComponent
+    },
+    {
+        path:'admin/categories/add',
+        component:AddCategoryComponent
+    }
+];
