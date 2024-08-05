3dc01d6f0e89e7b253392d703ce2bae60a99ba4c
Rahulpatel1217 <send2rahul1217@gmail.com>
2024-08-05 20:16:09 +0530

    Commit

--- src/app/Feature/category/Services/category.service.ts
+++ src/app/Feature/category/Services/category.service.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { Addcategoryrequist } from '../Model/add-category-requist-model';
 import { category } from '../Model/category-model';
 import { environment } from '../../../../environments/environment.development';
+import { UpdateCategory } from '../Model/UpdateCategoryModel';
 
 @Injectable({
   providedIn: 'root'
@@ -20,5 +21,12 @@ export class CategoryService {
   getAllcategories(): Observable<category[]>{
 
     return this.http.get<category[]>(`${environment.apiBaseUrl}/api/Categories`);
+  }
+
+  getcategoriesByID(id: string): Observable<category>{
+    return this.http.get<category>(`${environment.apiBaseUrl}/api/Categories/${id}`);
+  }
+  updatecategories(id: string,updatecategory:UpdateCategory): Observable<category>{
+    return this.http.put<category>(`${environment.apiBaseUrl}/api/Categories/${id}`,updatecategory);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
+import { CommonModule } from "@angular/common";
 
 
 @NgModule({
 declarations:[
 ],
 imports:[
-    BrowserModule
+    BrowserModule,
+    CommonModule
 ],
 providers:[
 
